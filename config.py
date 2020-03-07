775dfad5692ac5186e406444bac03e7a2f78bcbe
Tagir <killerbeesexy@gmail.com>
2020-03-07 20:17:14 +0300

    fjfkfkfkfk.

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ from decouple import config
 from peewee import SqliteDatabase
 from playhouse.db_url import connect
 
-DB = connect(config("DB_URL", default='sqlite:///bot.db'))
+DB = connect(config("DATABASE_URL", default='sqlite:///bot.db'))
 TOKEN = config('TOKEN')
 port = config('port', default=8443)
 HEROKU_APP_NAME = config('HEROKU_APP_NAME', default=None)
+psycopg2-binary==2.8.4
