02fb4e52d7943d245a5e1288c8b08d458877ac46
Tagir <killerbeesexy@gmail.com>
2020-03-10 13:42:40 +0300

    yeee

--- killroyka.py
+++ killroyka.py
@@ -1,19 +1,3 @@
-# -*- coding: utf-8 -*-
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 import logging
 import telegram
 import config
@@ -53,7 +37,7 @@ b = []
 
 
 def generate_password(update, context: CallbackContext):
-    for x in range(context.args[0]):
+    for x in range(int(context.args[0])):
         b.append(choice(a))
     c = ''.join(b)
     b.clear()
