8afcc2f2ca0cb74559d65a3ca7dc78eb084573bd
Tagir <killerbeesexy@gmail.com>
2020-07-10 13:36:30 +0300

    ;jdfh;gashr;rguh;aoe

--- killroyka.py
+++ killroyka.py
@@ -185,6 +185,5 @@ def main():
                               url_path=config.TOKEN)
         updater.bot.set_webhook(f"https://{config.HEROKU_APP_NAME}.herokuapp.com/{config.TOKEN}")
 
-save()
 if __name__ == '__main__':
     main()
